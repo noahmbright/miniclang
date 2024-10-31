a32eb6905d1e8deffb03bca57e22763ec62713f5
Noah Bright <noah.bright.1@gmail.com>
2024-10-31 16:24:16 -0400

    read file in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,44 @@
-#include <iostream>
+#include <cstdlib>
+#include <stdio.h>
+#include <unistd.h>
 
-int main() {
-  std::cout << "Hello from miniclang. This program doesn't do anything yet. "
-               "Come back again and it might!";
+// from crafting interpreters, ch 16
+static char *read_file(const char *path) {
+  FILE *file = fopen(path, "r");
+  if (file == NULL) {
+    fprintf(stderr, "Could not open file %s, aborting.\n", path);
+  }
+
+  fseek(file, 0L, SEEK_END);
+  long file_size = ftell(file);
+  rewind(file);
+
+  char *buffer = (char *)malloc(file_size + 1);
+  if (buffer == NULL) {
+    fprintf(stderr, "Could not buffer file %s, aborting.\n", path);
+  }
+
+  long bytes_read = fread(buffer, sizeof(char), file_size, file);
+  if (bytes_read < file_size) {
+    fprintf(stderr, "Could not read file %s, aborting.\n", path);
+  }
+
+  buffer[bytes_read] = '\0';
+
+  fclose(file);
+  return buffer;
+}
+
+int main(int argc, char **argv) {
+
+  for (int i = 1; i < argc; i++) {
+    if (access(argv[i], F_OK) == 0) {
+      char *buffer = read_file(argv[i]);
+      (void)buffer;
+    } else {
+      fprintf(stderr, "File %s not found, aborting.\n", argv[i]);
+    }
+  }
+
   return 0;
 }
